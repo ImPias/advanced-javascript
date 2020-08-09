6e4bb7e1d09063dacb2a6f7740ea86d3dfb550d2
impias <kazipias123@gmail.com>
2020-08-10 01:05:33 +0600

    Null vs Undefiend

---
+++ null-vs-undefiend.js
@@ -0,0 +1,31 @@
+// Method-0
+let name;
+console.log(name);
+
+// Method-1
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+
+const result1 = add(10, 22);
+console.log(result1);
+
+// Method-2
+function sub(num1, num2){
+    console.log(num1, num2);
+}
+
+const result2 = sub(30);
+console.log(result2);
+
+// Method-3
+const check = undefined;
+console.log(check);
+
+// Method-4
+const friends = ['mainul', 'delowar', 'faruk', 'saiful', 'israfil'];
+console.log(friends[7]);
+
+// Method-5
+const profile = {name: "Pias", age: 23, company: "Antaranga Dot Com Ltd."};
+console.log(profile.salary);
