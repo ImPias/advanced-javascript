5de009f891d50a22d2ea8348eb7ce9942418758c
impias <kazipias123@gmail.com>
2020-08-10 04:14:52 +0600

    Slice Splice Join

---
+++ array-split.js
@@ -0,0 +1,13 @@
+const cars = ["Audi", "BMW", "Ferari", "Jaguar", "Mazda", "Toyota"];
+console.log(cars);
+
+const slice = cars.slice(1, 4);
+console.log(slice);
+console.log(cars);
+
+const splice = cars.splice(2, 2);
+console.log(splice);
+console.log(cars);
+
+const join = cars.join(" ");
+console.log(join);
