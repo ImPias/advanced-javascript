41d27add9450b8c4a049e518fbfd14b03c0998e9
impias <kazipias123@gmail.com>
2020-08-10 01:07:02 +0600

    Double equal vs Triple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,31 @@
+let num1 = 2;
+let num2 = "2";
+if(num1 == num2){
+    console.log("Condition is true.");
+} else {
+    console.log("Condition is false.");
+}
+
+let num3 = 2;
+let num4 = "2";
+if(num3 === num4){
+    console.log("Condition is true.");
+} else {
+    console.log("Condition is false.");
+}
+
+let num5 = 1;
+let num6 = true;
+if(num5 == num6){
+    console.log("Condition is true.");
+} else {
+    console.log("Condition is false.");
+}
+
+let num7 = 1;
+let num8 = true;
+if(num7 === num8){
+    console.log("Condition is true.");
+} else {
+    console.log("Condition is false.");
+}
