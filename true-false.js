ac015ee39c2fc49f7cb8e32b2d0a1b40da32589a
impias <kazipias123@gmail.com>
2020-08-10 00:23:15 +0600

    True False

---
+++ true-false.js
@@ -0,0 +1,21 @@
+// Falsy
+// false
+// 0
+// ""
+// undefined
+// Nan
+// Null
+
+// Truthy
+// "0"
+// " "
+// let number;
+// console.log(number);
+
+const name = "0";
+
+if(name){
+    console.log("Condition is True");
+} else {
+    console.log("Condition is False");
+}
